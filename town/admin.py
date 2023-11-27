11fb315b70a3e44c1085b5c08b1104c4f86157ef
Islamiddin Islamov <islamov.isl168@gmail.com>
2023-11-27 11:16:46 +0600

    added Announcement in models

--- town/admin.py
+++ town/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from town.models import Announcement
 
-# Register your models here.
+
+admin.site.register(Announcement)
--- town/models.py
+++ town/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from town.upload_image import upload_image
+
+
+class Announcement(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Объявления')
+    date = models.DateField(verbose_name='Дата публикации', default=timezone.now)
+    text = models.TextField(verbose_name='Текст публикации', blank=True)
+    file = models.FileField(verbose_name='Файл', upload_to='media/', blank=True)
+    image = models.ImageField(verbose_name='Изображение',
+                              upload_to=upload_image, blank=True)
+
+    class Meta:
+        verbose_name = 'Объявления'
+        verbose_name_plural = 'Объявления'
+
+    def __str__(self):
+        return self.title
