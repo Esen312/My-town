3f650537179d486175c9349f0a224ee18fd9aaed
Islamiddin Islamov <islamov.isl168@gmail.com>
2023-12-04 05:26:52 +0600

    chanched header

--- templates/assets/js/header.js
+++ templates/assets/js/header.js
@@ -1,20 +1,18 @@
-const dropdown = document.querySelector('.dropdown');
+<!-- Include this script in your HTML -->
+    // Function to toggle the language dropdown
+    function toggleDropdown() {
+        document.getElementById("languageDropdown").classList.toggle("show");
+    }
 
-dropdown.addEventListener('click', () => {
-    dropdown.classList.toggle('active');
-});
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    const searchIcon = document.getElementById("search-icon");
-    const searchForm = document.querySelector(".search");
-
-    searchIcon.addEventListener("click", function () {
-        searchForm.classList.toggle("active");
-        if (searchForm.classList.contains("active")) {
-            searchIcon.innerHTML = '<i class="bx bx-x"></i>'; // Change to close icon
-        } else {
-            searchIcon.innerHTML = '<i class="bx bx-search"></i>'; // Change back to search icon
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            for (var i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
         }
-    });
-});
+    }
--- town/translation.py
+++ town/translation.py
@@ -1,5 +1,5 @@
 from modeltranslation.translator import register, TranslationOptions
-from .models import Announcement, TownHallManagement, History
+from .models import Announcement, TownHallManagement, History, News
 
 
 @register(Announcement)
