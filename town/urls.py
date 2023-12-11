e1c273ca60b723c87ebf474a3c81bcca174d112d
Islamiddin Islamov <islamov.isl168@gmail.com>
2023-12-11 10:33:33 +0600

    updated views.py

--- town/urls.py
+++ town/urls.py
@@ -1,8 +1,6 @@
 from .views import (NewsListView, NewsDetailView, feedback, contact_view, AnnouncementListView, AnnouncementDetailView,
                     OfficialDocumentsListView, OfficialDocumentsDetailView, HistoryPage, TownHallManagementListView,
                     search_view, index, passport_of_town, vacancy, MapView)
-
-
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
